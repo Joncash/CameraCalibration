a43cb528225570b3580ab9186124b54320c5f0b9
yeegaka <yeegaka@gmail.com>
2013-12-21 16:34:40 +0800

    (^) init Settings TAB UI

--- App.Calibration/CalibrationForm.Designer.cs
+++ App.Calibration/CalibrationForm.Designer.cs
@@ -32,7 +32,20 @@
             this.SettingsTabPage = new System.Windows.Forms.TabPage();
             this.CalibrationTabPage = new System.Windows.Forms.TabPage();
             this.ResultTabPage = new System.Windows.Forms.TabPage();
+            this.expandCollapsePanel1 = new MakarovDev.ExpandCollapsePanel.ExpandCollapsePanel();
+            this.expandCollapsePanel2 = new MakarovDev.ExpandCollapsePanel.ExpandCollapsePanel();
+            this.expandCollapsePanel3 = new MakarovDev.ExpandCollapsePanel.ExpandCollapsePanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.CalibrationTab.SuspendLayout();
+            this.SettingsTabPage.SuspendLayout();
+            this.expandCollapsePanel1.SuspendLayout();
+            this.expandCollapsePanel2.SuspendLayout();
+            this.expandCollapsePanel3.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // CalibrationTab
@@ -49,6 +62,7 @@
             // 
             // SettingsTabPage
             // 
+            this.SettingsTabPage.Controls.Add(this.flowLayoutPanel1);
             this.SettingsTabPage.Location = new System.Drawing.Point(4, 22);
             this.SettingsTabPage.Name = "SettingsTabPage";
             this.SettingsTabPage.Padding = new System.Windows.Forms.Padding(3);
@@ -76,6 +90,100 @@
             this.ResultTabPage.Text = "結果";
             this.ResultTabPage.UseVisualStyleBackColor = true;
             // 
+            // expandCollapsePanel1
+            // 
+            this.expandCollapsePanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.expandCollapsePanel1.ButtonSize = MakarovDev.ExpandCollapsePanel.ExpandCollapseButton.ExpandButtonSize.Normal;
+            this.expandCollapsePanel1.ButtonStyle = MakarovDev.ExpandCollapsePanel.ExpandCollapseButton.ExpandButtonStyle.Circle;
+            this.expandCollapsePanel1.Controls.Add(this.radioButton1);
+            this.expandCollapsePanel1.ExpandedHeight = 0;
+            this.expandCollapsePanel1.IsExpanded = true;
+            this.expandCollapsePanel1.Location = new System.Drawing.Point(3, 3);
+            this.expandCollapsePanel1.Name = "expandCollapsePanel1";
+            this.expandCollapsePanel1.Size = new System.Drawing.Size(987, 72);
+            this.expandCollapsePanel1.TabIndex = 0;
+            this.expandCollapsePanel1.Text = "Calibration Task";
+            this.expandCollapsePanel1.UseAnimation = true;
+            // 
+            // expandCollapsePanel2
+            // 
+            this.expandCollapsePanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.expandCollapsePanel2.ButtonSize = MakarovDev.ExpandCollapsePanel.ExpandCollapseButton.ExpandButtonSize.Normal;
+            this.expandCollapsePanel2.ButtonStyle = MakarovDev.ExpandCollapsePanel.ExpandCollapseButton.ExpandButtonStyle.Circle;
+            this.expandCollapsePanel2.Controls.Add(this.label2);
+            this.expandCollapsePanel2.Controls.Add(this.label1);
+            this.expandCollapsePanel2.ExpandedHeight = 0;
+            this.expandCollapsePanel2.IsExpanded = true;
+            this.expandCollapsePanel2.Location = new System.Drawing.Point(3, 81);
+            this.expandCollapsePanel2.Name = "expandCollapsePanel2";
+            this.expandCollapsePanel2.Size = new System.Drawing.Size(987, 119);
+            this.expandCollapsePanel2.TabIndex = 2;
+            this.expandCollapsePanel2.Text = "Calibration Plate";
+            this.expandCollapsePanel2.UseAnimation = true;
+            // 
+            // expandCollapsePanel3
+            // 
+            this.expandCollapsePanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.expandCollapsePanel3.ButtonSize = MakarovDev.ExpandCollapsePanel.ExpandCollapseButton.ExpandButtonSize.Normal;
+            this.expandCollapsePanel3.ButtonStyle = MakarovDev.ExpandCollapsePanel.ExpandCollapseButton.ExpandButtonStyle.Circle;
+            this.expandCollapsePanel3.Controls.Add(this.label3);
+            this.expandCollapsePanel3.ExpandedHeight = 0;
+            this.expandCollapsePanel3.IsExpanded = true;
+            this.expandCollapsePanel3.Location = new System.Drawing.Point(3, 206);
+            this.expandCollapsePanel3.Name = "expandCollapsePanel3";
+            this.expandCollapsePanel3.Size = new System.Drawing.Size(987, 121);
+            this.expandCollapsePanel3.TabIndex = 3;
+            this.expandCollapsePanel3.Text = "Camera Parameters";
+            this.expandCollapsePanel3.UseAnimation = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.expandCollapsePanel1);
+            this.flowLayoutPanel1.Controls.Add(this.expandCollapsePanel2);
+            this.flowLayoutPanel1.Controls.Add(this.expandCollapsePanel3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(8, 18);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(990, 510);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(35, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(292, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Full Calibration:Pose and all Camera Parameters";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Description File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Thickness";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Camera Model";
+            // 
             // CalibrationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -85,6 +193,14 @@
             this.Name = "CalibrationForm";
             this.Text = "Camera Calibration";
             this.CalibrationTab.ResumeLayout(false);
+            this.SettingsTabPage.ResumeLayout(false);
+            this.expandCollapsePanel1.ResumeLayout(false);
+            this.expandCollapsePanel1.PerformLayout();
+            this.expandCollapsePanel2.ResumeLayout(false);
+            this.expandCollapsePanel2.PerformLayout();
+            this.expandCollapsePanel3.ResumeLayout(false);
+            this.expandCollapsePanel3.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -95,6 +211,14 @@
         private System.Windows.Forms.TabPage SettingsTabPage;
         private System.Windows.Forms.TabPage CalibrationTabPage;
         private System.Windows.Forms.TabPage ResultTabPage;
+        private MakarovDev.ExpandCollapsePanel.ExpandCollapsePanel expandCollapsePanel1;
+        private MakarovDev.ExpandCollapsePanel.ExpandCollapsePanel expandCollapsePanel2;
+        private MakarovDev.ExpandCollapsePanel.ExpandCollapsePanel expandCollapsePanel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
