d740748dcd76ce5adcc0f757fb656729736cac50
joncash <tw.kchsieh@gmail.com>
2014-01-06 00:22:01 +0800

    (^) 新增鏡頭參數

--- CalibrationModels/CameraParam.cs
+++ CalibrationModels/CameraParam.cs
@@ -10,10 +10,62 @@ namespace CalibrationModels
 	/// </summary>
 	public class CameraParam
 	{
-		public string CameraType { get; set; }
-		public double Sx { get; set; }
-		public double Sy { get; set; }
-		public double Focus { get; set; }
+		public CameraParam()
+		{
+			//default value
+			Kappa = K1 = K2 = K3 = P1 = P2 = 0.0;
+
+			Vx = Vz = 0;
+			Vy = 500.0;
+		}
+		public CameraModelType CameraType { get; set; }
 		public bool IsTelecentric { get; set; }
+
+		#region Common param
+		/// <summary>
+		/// Cell Width
+		/// </summary>
+		public double Sx { get; set; }
+
+		/// <summary>
+		/// Cell Height
+		/// </summary>
+		public double Sy { get; set; }
+
+		/// <summary>
+		/// FocalLength
+		/// </summary>
+		public double Focus { get; set; }
+		#endregion
+
+		#region AreaScanDivision param
+		public double Kappa { get; set; }
+		#endregion
+
+		#region AreaScanPolynomial param
+		public double K1 { get; set; }
+		public double K2 { get; set; }
+		public double K3 { get; set; }
+		public double P1 { get; set; }
+		public double P2 { get; set; }
+		#endregion
+
+		#region LineScan Param
+		/// <summary>
+		/// Motion Vector X
+		/// </summary>
+		public double Vx { get; set; }
+
+		/// <summary>
+		/// Motion Vector Y
+		/// </summary>
+		public double Vy { get; set; }
+
+		/// <summary>
+		/// Motion Vector Z
+		/// </summary>
+		public double Vz { get; set; }
+		#endregion
+
 	}
 }
