9542314e23df4284c217bb4a4446930de821186a
joncash <tw.kchsieh@gmail.com>
2014-02-05 10:38:16 +0800

    CalibrationWorker (^) 新增內部用的 ReportModel (^) Timer 負負分派工作的方式修正

--- CalibrationModule/CalibrationWorker.cs
+++ CalibrationModule/CalibrationWorker.cs
@@ -8,6 +8,13 @@ using System.Text;
 namespace CalibrationModule
 {
 	public delegate void CalibrationImageCalculatorEventHandler(object sender, CalibrationEventArgs e);
+
+	/// <summary>
+	/// 執行取得影像報告工作
+	/// 1。將工作放入工作佇列中
+	/// 2。依序取得影像報告
+	/// 3。回覆報告
+	/// </summary>
 	public class CalibrationWorker
 	{
 		#region private variables
@@ -15,7 +22,8 @@ namespace CalibrationModule
 		private CalibImageCalculator _calibImageCalculator;
 		private CalibrationAssistant _assistant;
 		private List<object> _workingStack;
-		System.Windows.Forms.Timer _timer;
+		private System.Windows.Forms.Timer _timer;
+		private int _accWorkCount; // 累計工作量
 		#endregion
 
 		#region public Delegate
@@ -48,7 +56,8 @@ namespace CalibrationModule
 		public void RequestCalibrationImageReport(CalibImageDTO imageDTO)
 		{
 			_workingStack.Add(imageDTO);
-			_timer.Start();
+			if (!_timer.Enabled)
+				_timer.Start();
 		}
 
 		#endregion
@@ -68,57 +77,90 @@ namespace CalibrationModule
 			_timer.Tick += _timer_Tick;
 		}
 
-		void _timer_Tick(object sender, EventArgs e)
+		private void _timer_Tick(object sender, EventArgs e)
 		{
 			if (_workingStack.Count == 0)
 			{
-				stop();
+				stopTimer();
 			}
 			else if (!_bg.IsBusy)
 			{
 				var model = _workingStack[0];
 				_bg.RunWorkerAsync(model);
+				stopTimer();
 			}
 		}
 
-		private void stop()
+		private void stopTimer()
 		{
 			_timer.Stop();
 		}
-		void _bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+
+		private void _bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
 		{
 			if (e.Cancelled)
 			{
 				//ToDo
 			}
+			else
+			{
+				//分派下一個工作，如果有的話
+				_timer.Start();
+			}
 		}
 
 		void _bg_ProgressChanged(object sender, ProgressChangedEventArgs e)
 		{
-			if (ResponseCalibrationReport != null)
+			var model = e.UserState as ReportModel;
+			if (ResponseCalibrationReport != null && model != null)
 			{
-				object model = "";
-				var args = new CalibrationEventArgs(model);
-				ResponseCalibrationReport(sender, args);
+				CalibrationAssistant.EventNotifyType notifyType;
+				if (Enum.TryParse<CalibrationAssistant.EventNotifyType>(model.UserState, out notifyType))
+				{
+					var args = new CalibrationEventArgs(model)
+					{
+						EventType = notifyType,
+					};
+					ResponseCalibrationReport(sender, args);
+				}
 			}
 		}
 
 		void _bg_DoWork(object sender, DoWorkEventArgs e)
 		{
 			var worker = sender as BackgroundWorker;
-			var data = e.Argument as CalibImageDTO;
-			if (data != null)
+			var model = e.Argument as CalibImageDTO;
+			if (model != null)
 			{
-				//data
+				//prepare data
 				var descFile = _assistant.GetCalibPlateDescrFile();
-				var calibImage = _assistant.GetCalibImage(data.ImageID);
+				var calibImage = _assistant.GetCalibImage(model.ImageID);
 				var plateParam = _assistant.GetCalibrationPlateParam();
 				var startCameraParam = _assistant.GetCameraParamHTuple(calibImage);
-				var plateMarkModel = _calibImageCalculator.GetCalibrationPlateMarkViewModel(descFile, calibImage.GetImage()
-																				, plateParam, startCameraParam);
-				//_bg.ReportProgress(1, "");
+
+				//doWork
+				var report = _calibImageCalculator.GetCalibtionImageReport(descFile, calibImage, plateParam, startCameraParam);
+
+				//var plateMarkModel = _calibImageCalculator.GetCalibrationPlateMarkViewModel(descFile, calibImage.GetImage()
+				//																, plateParam, startCameraParam);
+
+				var reportModel = new ReportModel()
+				{
+					UserState = model.UserState,
+					Model = report,
+				};
+				_bg.ReportProgress(++_accWorkCount, reportModel);
+
+				//remove work
+				_workingStack.RemoveAt(0);
 			}
 		}
 		#endregion
+
+		class ReportModel
+		{
+			public string UserState { get; set; }
+			public object Model { get; set; }
+		}
 	}
 }
